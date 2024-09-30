00a6cb13b3d4b5fc0c0e14651f17c9226c73218c
Peeyush <69499613+PeeyushSachan@users.noreply.github.com>
2024-09-30 17:18:14 +0530

    How we can add logs in our project

--- lib/data/local/db_helper.dart
+++ lib/data/local/db_helper.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'dart:developer'; // for creating logs
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
@@ -21,9 +21,9 @@ class DBHelper {
   Future<Database> getDb() async {
     if (myDB == null) {
       myDB = await openDb();
-      stdout.write("DatBase is not availabe but created");
     } else {
-      stdout.write("DatBase is available");
+      
+      log("Database is available");
     }
 
     return myDB!;
@@ -37,10 +37,13 @@ class DBHelper {
 
     /// path data/data/yourPackageName/databases/notes.db
     String dbPath = join(rootPath, "notes.db");
-
+        log("Database Created");
+        log("Table is Created");
     return await openDatabase(dbPath, version: 1, onCreate: (db, version) {
+     
       db.rawQuery(
           "create table $notesDataTable($s_no integer primary key autoincrement, $Columntitle text, $columndescription text)");
+      
     });
   }
 
@@ -52,6 +55,8 @@ class DBHelper {
         .insert(notesDataTable, {Columntitle: title, columndescription: desc});
 
     stdout.write("method is called return: $rowsEffected");
+    log("Data is Inserted");
+
     return rowsEffected > 0;
   }
 
@@ -59,45 +64,42 @@ class DBHelper {
   Future<List<Map<String, dynamic>>> getAllNotes() async {
     var db = await getDb();
     var allNotes = await db.query(notesDataTable);
+    log("Data is Fetching");
 
     return allNotes;
   }
 
   ///delete the specific data
- Future<int> deleteNotes({required int rowIndex}) async {
-  var db = await getDb();
+  Future<int> deleteNotes({required int rowIndex}) async {
+    var db = await getDb();
 
-  // Use rawDelete for delete operations
-  int deletedCount = await db.rawDelete(
-      "DELETE FROM $notesDataTable WHERE s_no = ?", [rowIndex]);
+    // Use rawDelete for delete operations
+    int deletedCount = await db
+        .rawDelete("DELETE FROM $notesDataTable WHERE s_no = ?", [rowIndex]);
+    log("Data is Deleted");
 
-  return deletedCount; // Returns the number of rows affected
-}
-
+    return deletedCount; // Returns the number of rows affected
+  }
 
   ///Update the specific data
 
-Future<int> updateNotes({
-  required int rowIndex,
-  required String rowTitle,
-  required String rowDescription,
-}) async {
-  var db = await getDb();
+  Future<int> updateNotes({
+    required int rowIndex,
+    required String rowTitle,
+    required String rowDescription,
+  }) async {
+    var db = await getDb();
 
-  // Use rawUpdate for update operations
-  int updatedCount = await db.rawUpdate(
-    "UPDATE $notesDataTable SET $Columntitle = ?, $columndescription = ? WHERE s_no = ?",
-    [rowTitle, rowDescription, rowIndex]
-  );
+    // Use rawUpdate for update operations
+    int updatedCount = await db.rawUpdate(
+        "UPDATE $notesDataTable SET $Columntitle = ?, $columndescription = ? WHERE s_no = ?",
+        [rowTitle, rowDescription, rowIndex]);
 
-  return updatedCount; // Returns the number of rows affected
-}
+    return updatedCount; // Returns the number of rows affected
+  }
 /*
 Used rawUpdate instead of rawQuery.
 Returned int to indicate the number of rows updated.
 Used placeholders (?) to safely pass values and avoid SQL injection risks.
 Removed the incorrect from keyword from the UPDATE query.*/
-
 }
-
-
